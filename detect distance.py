7036fb2816e37d35a50c26d2006aaa5d0120c23c
Jason Baer <75148446+jmbaer@users.noreply.github.com>
2022-10-13 17:40:23 -0400

    added RGB print to terminal on hover

--- detect distance.py
+++ detect distance.py
@@ -1,3 +1,4 @@
+from turtle import color
 import cv2
 import pyrealsense2
 from realsense_depth import *
@@ -7,6 +8,7 @@ point = (400, 300)
 def show_distance(event, x, y, args, params):
     global point
     point = (x, y)
+    print(color_frame[y][x])
 
 # Initialize Camera Intel Realsense
 dc = DepthCamera()
@@ -23,6 +25,7 @@ while True:
     distance = depth_frame[point[1], point[0]]
 
     cv2.putText(color_frame, "{}mm".format(distance), (point[0], point[1] - 20), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
+
 
     cv2.imshow("depth frame", depth_frame)
     cv2.imshow("Color frame", color_frame)
